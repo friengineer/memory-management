92192eb74bc82572c5795467f9f85f6357c9c0ec
Reece Dudley <ll16rdjd@leeds.ac.uk>
2018-11-07 21:56:24 +0000

    Started work on function to print out the data for each block.

--- memory_management.c
+++ memory_management.c
@@ -5,10 +5,10 @@
 #include "memory_management.h"
 
 // pointers for the beginning and end of the memory and for the end of the last page
-void *head;
-Block *tail;
-int pageEnd;
-bool pageCreated = false;
+Block *head = NULL;
+Block *tail = NULL;
+// int pageEnd;
+// bool pageCreated = false;
 
 
 void * addBlock(Block block){  // change size of block and change address of next block
@@ -23,9 +23,28 @@ void newPage(int number){
 
     sbrk(4096);
     printf("Created new page\n");
-    printf("-------------------");
-    pageEnd = sbrk(0);
+    // pageEnd = sbrk(0);
   }
+}
+
+void display(){
+
+  // Block *temp;
+
+  // temp = head->nextBlock;
+
+  if (block->prevBlock == NULL){
+
+    printf("End\n\n");
+  } else (block->prevBlock != NULL){
+
+    display(block->prevBlock);
+  }
+
+  printf("Size: %d\n", block->size);
+  printf("Free: %d\n", block->isFree);
+  printf("Next: %d\n", block->nextBlock);
+  printf("Prev: %d\n\n", block->prevBlock);
 }
 
 // allocates the specified amount of bytes in memory
@@ -33,82 +52,65 @@ void * _malloc(size_t size){
 
   if (size == 0){
 
-    printf("Input is 0\n");
     return NULL;
   }
 
   // changes what the value of prevBlock is and the values stored in the previous block
   // depending on whether a block has been created or not
-  if (tail == head && pageCreated == false){
+  if (head == NULL && tail == NULL){      // don't need head part??
 
-    if (((size + sizeof(Block))%4096) == 0){
+    head = sbrk(4096);
+    head->size = 0;
+    head->isFree = false;
+    head->nextBlock = NULL;
+    head->prevBlock = NULL;
+    tail = (int)head + sizeof(Block);
+    tail->size=0;
+    tail->isFree = false;
+    tail->nextBlock = NULL;
+    tail->prevBlock = NULL;
+  }
 
-      printf("Multiple of 4096\n");
-      newPage((size/4096));
-    }
-    else{
+  if (tail == ((int)head + sizeof(Block))){
 
-      printf("Not a multiple of 4096\n");
-      newPage((size/4096) + 1);
-    }
-
-    pageCreated = true;
+    Block *block = (int)tail + sizeof(Block);
+    block->prevBlock = NULL;
+    block->size = size;
+    block->isFree = false;
+    // there is no block after the new block
+    block->nextBlock = NULL;
+    // head =
+    display();
+    tail = (int)tail + sizeof(Block);
+    tail->size = block->size;
+    tail->isFree = block->isFree;
+    tail->nextBlock = block->nextBlock;
+    tail->prevBlock = block->prevBlock;
   }
   else{
 
-    printf("%d\n", sizeof(tail));
-    int newMemoryEnd = tail + tail->size + size + sizeof(Block);       // adding tail->size to anything is causing problem
-    printf("%d\n", newMemoryEnd);
-
-    // checks if there is room for the new block in the current page and if not, asks for
-    // the required number of new pages
-    if (newMemoryEnd > pageEnd){
-
-      int bytesRequired = newMemoryEnd - (int)pageEnd;
-
-      if ((bytesRequired%4096) == 0){
-
-        printf("Multiple of 4096\n");
-        newPage((bytesRequired/4096));
-      }
-      else{
-
-        printf("Not a multiple of 4096\n");
-        newPage((bytesRequired/4096) + 1);
-      }
-    }
+    Block *block = (int)tail + sizeof(Block) + size;
+    block->prevBlock = (int)tail;
+    block->size = size;
+    block->isFree = false;
+    // there is no block after the new block
+    block->nextBlock = NULL;
+    // tail->nextBlock = (int)tail + tail->size;
+    display();
+    tail = (int)tail + sizeof(Block) + size;
+    tail->size = block->size;
+    tail->isFree = block->isFree;
+    tail->nextBlock = block->nextBlock;
+    tail->prevBlock = block->prevBlock;
   }
 
-  Block * block;
-
-  // block size include data and the block header
-  block->size = size + sizeof(Block);
-  block->isFree = false;
-  // there is no block after the new block
-  block->nextBlock = NULL;
-
-  if (tail == head){
-
-    block->prevBlock = head;
-  }
-  else{
-
-    block->prevBlock = tail;
-    tail->nextBlock = tail + tail->size;
-    printf("%d\n", tail->nextBlock);
-    tail = tail->nextBlock;
-  }
-
-  printf("Block size: %ld\n", (long)block->size);
-
-  printf("%d\n", block->nextBlock);
-
-  printf("%d\n", sbrk(0));
+  // display(head);
+  display();
+  // display(block);
 
   printf("Here\n");
-  printf("%d\n", tail);
   // returns pointer for the address of the created block
-  return tail;
+  return (int)tail + sizeof(Block);
 }
 
 void _free(void * ptr){
@@ -127,20 +129,27 @@ void _free(void * ptr){
 
 void main(){
 
-  head = sbrk(0);  // returns beggining of the page
-  tail = head;
-  pageEnd = head;
-  printf("%d, %d\n", (size_t)100/4096, sizeof(Block));
-
+  // head = sbrk(0);  // returns beggining of the page
+  // tail = head;
+  // pageEnd = head;
 
   // printf("Head size: %d\nHead free: %d\nHead next: %d\nHead prev: %d\n", head->size, head->isFree, head->nextBlock, head->prevBlock);
   // printf("Tail size: %d\nTail free: %d\nTail next: %d\nTail prev: %d\n", tail->size, tail->isFree, tail->nextBlock, tail->prevBlock);
 
-  void * ptr = _malloc(0);
-  printf("%d\n", ptr);
+  // void * ptr = _malloc(0);
+  // printf("%d\n", ptr);
   // _free(ptr);
-  void * ptr2 = _malloc(100);
-  printf("%d\n", ptr2);
-  // _free(ptr2);
-  void * ptr3 = _malloc(200);
+
+  Block *blocky = _malloc(sizeof(Block)); // 32
+  Block *blocky2 = _malloc(sizeof(Block)); // 32
+  display();
+  display();
+  blocky->size = 50;
+  // int x = _malloc(sizeof(int));
+  // printf("%d\n", ptr2);
+  // display(ptr2);
+  // // _free(ptr2);
+  // void * ptr3 = _malloc(200);
+  // printf("%d\n", ptr3);
+  // display(ptr3);
 }
